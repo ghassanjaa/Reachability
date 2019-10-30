09620d367efc53d9ef28c914f59ce97c8698cf4b
Ghassan Jaam <ghassan_jaam@hotmail.com>
2019-10-30 13:43:24 +0200

    Check if network is Reachable

--- Reachability/ViewController.swift
+++ Reachability/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Reachablity is a simple framework to monitor the network state of an iOS device.
+    
+    //Add a Reachablity attribute to the view controller.
+    var reachability: Reachability!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Initialize the Reachablity object.
+        reachability = try! Reachability()
+        
+        //Check if we're reachable.
+        reachability.whenReachable = { reachability in
+            
+            //Return to the main thread after the reachablity of network is obtained.
+            DispatchQueue.main.async {
+                //Check if we are connected WiFi.
+                if reachability.connection == .wifi {
+                    print("connected via wifi")
+                } else {
+                    print("connected via cellular")
+                }
+            }
+        }
+        reachability.whenUnreachable = { reachability in
+            //Check if we're not reachable.
+            DispatchQueue.main.async {
+                print("not reachable")
+            }
+        }
+        
+        //After calling Reachability startNotifier, the Network status changes.
+        do {
+            try reachability.startNotifier()
+        } catch {
+            print("Unable to start notifier")
+        }
     }
-
-
+    
+    //Call stopNotifier() in deinit method for stopping notifications.
+    deinit {
+        reachability.stopNotifier()
+    }
 }
 
